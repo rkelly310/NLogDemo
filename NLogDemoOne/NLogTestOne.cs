ddd3949beb40334d3cd085b49e6560eff2d672b4
Kelly <ryan.kelly@accenture.com>
2021-03-16 11:31:39 -0400

    first commit

---
+++ NLogDemoOne/NLogTestOne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+
+namespace NLogDemoOne
+{
+    class NLogTestOne
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            logger.Error("This Is An Error Message!");
+        }
+    }
+}
