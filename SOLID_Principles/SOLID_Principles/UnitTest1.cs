fd71c8bf7601159e9401169f61e29702c057e4d1
Kelly <ryan.kelly@accenture.com>
2021-03-16 13:57:31 -0400

    added image

---
+++ SOLID_Principles/SOLID_Principles/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SOLID_Principles
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
